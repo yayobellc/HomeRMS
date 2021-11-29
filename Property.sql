4857d7bcd82942416705da4bed2dc67748ba20fc
Yoseph kassaye <yadugna@gmail.com>
2021-11-28 16:53:49 -0800

    initial Commit by YK

---
+++ Property.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Property]
+(
+	PropertyId INT NOT NULL PRIMARY KEY,
+	Address1 varchar(50), 
+	Address2 varchar(50),
+	City varchar(50),
+	[State] varchar(50),
+	Zip varchar(50)
+
+
+)
---
+++ Tenants.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Tenants]
+(
+	[TenantsId] INT NOT NULL PRIMARY KEY,
+	FName varchar(50),
+	LName varchar(50),
+	DOB datetime,
+	SSN varchar(50),
+	Address1 varchar(50),
+Address2 varchar(50),
+City varchar(50),
+[State] varchar(50)
+
+)
