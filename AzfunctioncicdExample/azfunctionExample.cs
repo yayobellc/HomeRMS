13d802e277007416c4979382ef4cd3b39ecb02e2
Yoseph kassaye <yadugna@gmail.com>
2021-12-18 11:01:10 -0800

    Modify the Category

---
+++ AzfunctioncicdExample/azfunctionExample.cs
@@ -0,0 +1,32 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace AzfunctioncicdExample
+{
+    public static class azfunctionExample
+    {
+        [FunctionName("azfunctionExample")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+          
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+
+            string responseMessage = "This HTTP triggered function executed successfully";
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
